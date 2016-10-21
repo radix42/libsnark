2dc978be13b3b21aa9e19aaa9986ce10590d86a2
David Mercer <radix42@gmail.com>
2016-10-21 15:54:20 -0700

    use modified profiling.cpp by @mugatu to work around lack of ::clock_gettime support on Mac and use ar from Xcode in Makefile

--- src/common/profiling.cpp
+++ src/common/profiling.cpp
@@ -26,6 +26,15 @@
 #include <proc/readproc.h>
 #endif
 
+#include <time.h>
+#include <sys/time.h>
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+
 namespace libsnark {
 
 long long get_nsec_time()
@@ -38,10 +47,26 @@ long long get_nsec_time()
 long long get_nsec_cpu_time()
 {
     ::timespec ts;
+    
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
+    
+#else
+//    clock_gettime(CLOCK_REALTIME, &ts);
+    
+    
     if ( ::clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts) )
         throw ::std::runtime_error("clock_gettime(CLOCK_PROCESS_CPUTIME_ID) failed");
         // If we expected this to work, don't silently ignore failures, because that would hide the problem and incur an unnecessarily system-call overhead. So if we ever observe this exception, we should probably add a suitable #ifdef .
         //TODO: clock_gettime(CLOCK_PROCESS_CPUTIME_ID) is not supported by native Windows. What about Cygwin? Should we #ifdef on CLOCK_PROCESS_CPUTIME_ID or on __linux__?
+#endif
+
     return ts.tv_sec * 1000000000ll + ts.tv_nsec;
 }
 
@@ -347,7 +372,7 @@ void print_compilation_info()
 {
 #ifdef __GNUC__
     printf("g++ version: %s\n", __VERSION__);
-    //printf("Compiled on %s %s\n", __DATE__, __TIME__);
+    printf("Compiled on %s %s\n", __DATE__, __TIME__);
 #endif
 #ifdef STATIC
     printf("STATIC: yes\n");
